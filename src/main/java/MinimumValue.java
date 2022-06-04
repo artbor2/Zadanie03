403c4fddd049e8e1f5b2aa07a6809a0728e5eca0
Artur Borkowski <105157280+artbor2@users.noreply.github.com>
2022-06-04 14:01:52 +0200

    Zadanie zaimplementowane

--- src/main/java/MinimumValue.java
+++ src/main/java/MinimumValue.java
@@ -1,5 +1,28 @@
 public class MinimumValue {
     public static void main(String[] args) {
 
+        int[] data = new int[] {3, 4, 9, 1, 6};
+        System.out.println("Najmniejsza liczba w tablic: " + minimum(data));
+        System.out.println("Najmniejsza liczba w tablic: " + minimum2(data));
+    }
+
+    public static int minimum(int[] data) {
+        int min = data[0];
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] < min) {
+                min = data[i];
+            }
+        }
+        return min;
+    }
+
+    public static int minimum2(int[] data) {
+        int min = data[0];
+        for (int number : data) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
     }
 }
